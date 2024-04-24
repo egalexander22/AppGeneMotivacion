9de3eb6e2d34d462a27b097f1727d1cd6701c628
Egalexander <egalexandermontalvo@outlook.com>
2024-04-24 08:31:47 -0500

    Agregar archivos de proyecto.

---
+++ AppGeneMotivacion/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppGeneMotivacion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}

689cd3399dd0c76b389e4de6711f5cbf82d10608
Egalexander <egalexandermontalvo@outlook.com>
2024-04-25 07:46:47 -0500

    actualizaciones

--- AppGeneMotivacion/MainPage.xaml.cs
+++ AppGeneMotivacion/MainPage.xaml.cs
@@ -2,23 +2,34 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private List<string> _quotes = new List<string>()
+        {
+            "El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
+            "No importa lo lento que vayas, siempre y cuando no te detengas.",
+            "La única forma de hacer un gran trabajo es amar lo que haces.",
+            "El fracaso es solo la oportunidad de comenzar de nuevo,esta vez de forma mas inteligente.",
+            "La vida es un 10% lo que me ocurre y un 90% como reacciono a ello.",
+            "Si puede soñarlo, puedes hacerlo.",
+        };
 
         public MainPage()
         {
             InitializeComponent();
+            Appearing += (sender, e) => StartScrollingAnimation();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnGenerateButtonClicked(object sender, EventArgs e)
         {
-            count++;
+            var random = new Random();
+            int index = random.Next(_quotes.Count);
+            quoteLabel.Text = _quotes[index];         
+        }
+        private void StartScrollingAnimation()
+        {
+            double length = scrollingLabel.Text.Length * 10;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            var animation = new Animation(callback: x => scrollingLabel.TranslationX = x, start: length, end: -length, easing: Easing.Linear);
+            animation.Commit(this, "StartScrollingAnimation", length: 50000, repeat: () => true);
         }
     }
 
